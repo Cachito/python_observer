b01c36745bc6b63f9b84c0721970f062ecdf7540
Luis Elías Carro <lcarro@globalnewsgroup.com>
2022-01-22 16:55:25 -0300

    commit 20220122 - 2
    otra característica inútil de python descubierta
    aunque no sé como deshabilitar un botón

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import QTimer
 from manager import ManagerView
 from views.observable import ObservableView
 
@@ -17,6 +18,12 @@ class ManagerWindow(QtWidgets.QMainWindow, ManagerView):
 
     @QtCore.pyqtSlot()
     def show_observable(self):
+        print(self.observable_windows)
+        print(len(self.observable_windows))
+
+        if(len(self.observable_windows) == 1):
+            return
+            
         observable_window = ObservableWindow()
         observable_window.show()
         self.observable_windows.append(observable_window)
